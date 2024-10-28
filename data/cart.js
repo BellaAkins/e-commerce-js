69ef32b7e2de0b905a1475a21375ddbcfac40573
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-10-28 16:46:53 -0700

    Update cart.js

--- data/cart.js
+++ data/cart.js
@@ -5,11 +5,13 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
+      deliveryOptionId: "1",
     },
 
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
+      deliveryOptionId: "2",
     },
   ];
 }
@@ -17,6 +19,7 @@ if (!cart) {
 function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
+
 export function addToCart(productId) {
   //Use productId as parameter cause it was defined outside the scope
   let matchingItem;
@@ -31,16 +34,19 @@ export function addToCart(productId) {
   );
 
   const quantity = Number(quantitySelector.value);
+
   if (matchingItem) {
     //   matchingItem.quantity += 1;
     matchingItem.quantity += quantity; //If matching item is in the cart increase it by quantity(which is the selector)
   } else {
     //if it is not in the cart push it
     cart.push({
-      theProductName: productId,
+      productId: productId,
       quantity: quantity,
+      deliveryOptionId: "1",
     });
   }
+
   saveToStorage();
 }
 
@@ -60,3 +66,46 @@ export function removeFromCart(productId) {
   cart = newCart;
   saveToStorage();
 }
+
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  return cartQuantity;
+}
+
+export function updateCartNewQuantity(productId, newQuantity) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem; //To check if product is already in the cart
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
+}
+
+/*
+function clearStorage() {
+  localStorage.clear();
+}
+
+clearStorage();
+*/
+//CHECK ON KEY AND TRY TO WRITE IT WITH FIND LIKE AI
