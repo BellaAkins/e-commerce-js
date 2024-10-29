54e8b330f5fd8a289d28f48057ed1f4ff403f64d
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-10-29 03:36:24 -0700

    Update deliveryoptions.js

--- data/deliveryoptions.js
+++ data/deliveryoptions.js
@@ -1,3 +1,4 @@
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 export const deliveryOptions = [
   {
     id: "1",
@@ -15,7 +16,6 @@ export const deliveryOptions = [
     priceCents: 999,
   },
 ];
-
 export function getDeliveryOption(deliveryOptionId) {
   let deliveryOption;
 
@@ -27,6 +27,41 @@ export function getDeliveryOption(deliveryOptionId) {
 
   return deliveryOption || deliveryOptions[0];
 }
+
+export function calculateDeliveryDate(deliveryOption) {
+  const today = dayjs();
+  const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+  const dateString = deliveryDate.format("dddd, MMMM, D");
+
+  return dateString;
+}
+
 /*
+
 return deliveryOptions//so it can be used outside the function
 deliveryOptions[0] //to use the 1st delivery option i.e the free one if we don't have a delivery option;*/
+//FOR WEEKEND
+/*
+function isWeekend() {
+  const dayOfWeek = date.format("dddd");
+  return dayOfWeek === "Saturday" || dayOfWeek === "Sunday";
+}
+export function calculateDeliveryDate(deliveryOption) {
+  // const today = dayjs();
+  // const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+  let remainingDays = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+
+  while (remainingDays > 0) {
+    deliveryDate = deliveryDate.add(1, "day");
+    if (!isWeekend(deliveryDate)) {
+      remainingDays--;
+    }
+
+    // remainingDays = remainingDays -1
+  }
+
+  const dateString = deliveryDate.format("dddd, MMMM, D");
+
+  return dateString;
+}*/
