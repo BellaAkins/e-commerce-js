19aee09691e6b21717959f6fdecd383d0718cd41
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-10-29 03:24:59 -0700

    Update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
+import { renderCheckOutHeader } from "./checkout scripts/checkoutheader.js";
 import { renderOrderSummary } from "./checkout scripts/ordersummary.js";
 import { renderPaymentSummary } from "./checkout scripts/paymentsummary.js";
-
+renderCheckOutHeader();
 renderOrderSummary();
 renderPaymentSummary();
